bb293114e171be46da4d37039ce11ffa2995ed1c
David Dornseifer <dornseid@amazon.com>
2025-03-19 14:39:32 +0100

    vCPUs advertisement for enclaves
    * Added separate device plugin to advise available vCPUs for enclaves which can be turned on via feature flag. K8s scheduler can pickup available vCPUs and schedule workloads based on vCPU availability.
    * Added config options to Readme and extended deployment manifest.
    * Added unit tests for config and extended unit tests for previous device plugin to test new functionality.

--- cmd/k8s-device-plugin/k8s-device-plugin.go
+++ cmd/k8s-device-plugin/k8s-device-plugin.go
@@ -7,6 +7,7 @@ import (
 	"flag"
 	"github.com/golang/glog"
 	"k8s-ne-device-plugin/pkg/config"
+	"k8s-ne-device-plugin/pkg/nitro_enclaves_cpu_plugin"
 	"k8s-ne-device-plugin/pkg/nitro_enclaves_device_monitor"
 	"k8s-ne-device-plugin/pkg/nitro_enclaves_device_plugin"
 	"os"
@@ -27,5 +28,17 @@ func main() {
 		os.Exit(1)
 	}
 
+	// create and start nitro enclave cpu device in background to advertise available cpus
+	if pluginConfig.EnclaveCPUAdvertisement {
+		go func() {
+			devicePluginCPU := nitro_enclaves_cpu_plugin.NewNitroEnclavesCPUDevicePlugin(pluginConfig)
+			if err := devicePluginCPU.Serve(); err != nil {
+				glog.Errorf("Error running the NE CPU plugin: %v", err)
+			}
+		}()
+	}
+
+	// start nitro enclave device plugin and start monitoring loop, main thread is active as long as enclave device
+	// plugin is running and healthy, otherwise terminate and have k8s restart container
 	monitor.Run()
 }
